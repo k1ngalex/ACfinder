e9aba36e6a80f20130594b4902cb98447c18e4a4
k1ngalex <alex.oastler@gmail.com>
2016-11-18 10:14:53 -0500

    done

---
+++ app/src/main/java/com/algonquincollege/king0410/acfinder/Constants/Constants.java
@@ -0,0 +1,14 @@
+package com.algonquincollege.king0410.acfinder.Constants;
+
+/**
+ * Created by AlexKing on 2016-11-17.
+ */
+
+
+
+public interface Constants {
+
+    public static final String PEMBROKE = "Algonquin College in the Valley";
+    public static final String PERTH = "7 Craig Street, Perth, ON, K7H 1X7";
+    public static final String WOODROFFE = "1385 Woodroffe Avenue, Ottawa, ON, K2G 1V8";
+}
